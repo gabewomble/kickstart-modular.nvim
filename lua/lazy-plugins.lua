24d71aea0139081236fccc113b8d97601bf00c09
Gabe Womble <womblg@amazon.com>
2024-03-01 09:35:36 -0800

    add tabs plugin

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -57,6 +57,8 @@ require('lazy').setup {
 
   require 'kickstart/plugins/comment',
 
+  require 'kickstart/plugins/tabs',
+
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- put them in the right spots if you want.
