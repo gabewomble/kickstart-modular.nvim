866220e2fc65d504fff7daff2294454ca662bca1
Gabe Womble <womblg@amazon.com>
2024-04-08 10:45:09 -0700

    add zen mode

---
+++ lua/custom/plugins/zen.lua
@@ -0,0 +1,7 @@
+return {
+  "folke/zen-mode.nvim",
+  opts = {
+    -- Leave empty to use the default configuration:
+    -- https://github.com/folke/zen-mode.nvim?tab=readme-ov-file#%EF%B8%8F-configuration
+  }
+}
